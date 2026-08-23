c70caeaad59c1c1a4aff832a49d0ae755c6c9267
ppul05 <pablo.pul2005@gmail.com>
2026-08-23 17:49:41 +0200

    Initial commit: MUX logic

---
+++ digital_thermostat.vhd
@@ -0,0 +1,24 @@
+
+-- The thermostat works with a MUX
+
+entity THERMUX is
+ port ( CURRENT_TEMP    : in bit_vector(6 downto 0);
+	DESIRED_TEMP   : in bit_vector(6 downto 0);
+	DISPLAY_SELECT : in bit; 
+	TEMP_DISPLAY   : out bit_vector (6 downto 0)
+ );
+ end THERMUX;
+
+architecture RTL of THERMUX is
+begin
+ process(CURRENT_TEMP,DESIRED_TEMP,DISPLAY_SELECT)
+  begin
+    if DISPLAY_SELECT = '1' then
+      TEMP_DISPLAY <= CURRENT_TEMP;
+    else
+      TEMP_DISPLAY <= DESIRED_TEMP;
+    end if;
+
+ end process;
+
+end RTL;
