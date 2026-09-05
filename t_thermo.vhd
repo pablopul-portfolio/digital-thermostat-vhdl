5d9dae552fe75f979239afe2ab3dad79946b4c27
ppul05 <pablo.pul2005@gmail.com>
2026-09-05 19:16:50 +0200

    Updated testbench for the fsm

--- t_thermo.vhd
+++ t_thermo.vhd
@@ -1,3 +1,6 @@
+Library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
 entity T_THERMO is
 
 end T_THERMO;
@@ -5,24 +8,28 @@ end T_THERMO;
 architecture TEST of T_THERMO is
 
  component THERMO is
-   port ( CURRENT_TEMP : in bit_vector(6 downto 0);
-	DESIRED_TEMP   : in bit_vector(6 downto 0);
-	DISPLAY_SELECT : in bit; 
-	COOL           : in bit;
-	HEAT           : in bit;
-	CLK            : in bit;
+   port ( CURRENT_TEMP   : in std_logic_vector(6 downto 0);
+	DESIRED_TEMP   : in std_logic_vector(6 downto 0);
+	DISPLAY_SELECT : in std_logic; 
+	COOL           : in std_logic;
+	HEAT           : in std_logic;
+	CLK	       : in std_logic;
+	FURNACE_HOT    : in std_logic;
+	AC_READY       : in std_logic;
 
-	TEMP_DISPLAY   : out bit_vector (6 downto 0);
-	A_C_ON         : out bit;
-	FURNACE_ON     : out bit
-   );
+	TEMP_DISPLAY   : out std_logic_vector (6 downto 0);
+	AC_ON          : out std_logic;
+	FURNACE_ON     : out std_logic;
+	FAN_ON         : out std_logic	
+ );
  end component;
 
-signal CURRENT_TEMP, DESIRED_TEMP     : bit_vector(6 downto 0);
-signal DISPLAY_SELECT                 : bit;
-signal TEMP_DISPLAY                   : bit_vector(6 downto 0);
-signal COOL, HEAT, A_C_ON, FURNACE_ON : bit;
-signal CLK                            : BIT;
+signal CURRENT_TEMP, DESIRED_TEMP     : std_logic_vector(6 downto 0);
+signal DISPLAY_SELECT                 : std_logic;
+signal TEMP_DISPLAY                   : std_logic_vector(6 downto 0);
+signal COOL, HEAT, AC_ON, FURNACE_ON : std_logic;
+signal FURNACE_HOT, AC_READY, FAN_ON  : std_logic;
+signal CLK                            : std_logic := '0';
 
 
 begin
@@ -33,10 +40,13 @@ begin
 		   DESIRED_TEMP => DESIRED_TEMP,
 		   DISPLAY_SELECT => DISPLAY_SELECT,
 		   TEMP_DISPLAY => TEMP_DISPLAY,
-		   A_C_ON => A_C_ON,
+		   AC_ON => AC_ON,
 		   HEAT => HEAT,
 		   COOL => COOL,
 		   FURNACE_ON => FURNACE_ON,
+		   FURNACE_HOT => FURNACE_HOT,
+		   AC_READY => AC_READY,
+		   FAN_ON => FAN_ON,
 		   CLK => CLK);
 
 process
@@ -49,15 +59,23 @@ process
  DISPLAY_SELECT <= '1';
  wait for 50 ns;
  HEAT <= '1';
- wait for 50 ns;
+ wait for 50 ns;  -- should stay in HEATON
+ FURNACE_HOT <= '1';
+ wait for 50 ns;  -- changes to FURNACEHOTNOW
  HEAT <= '0';
  wait for 50 ns;
+ FURNACE_HOT <= '0';  -- Should go back to IDLE state
+ wait for 50 ns; 
  CURRENT_TEMP <= "1000000";
  DESIRED_TEMP <= "0100000";
  wait for 50 ns;
  COOL <= '1';
+ wait for 50 ns; -- Changes to COOLON
+ AC_READY <= '1';
  wait for 50 ns;
  COOL <= '0';
+ wait for 50 ns;
+ AC_READY <= '0';  -- back to IDLE
  wait for 50 ns;
 
  wait;
